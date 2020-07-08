aab2d31a1519583f3c9b2c2158a917490586f30e
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-08 18:14:27 +0200

    .

---
+++ Tipos_datos/Booleanos.dart
@@ -0,0 +1,15 @@
+void main(){
+  //valores booleanos y declaraciones sencillas
+  bool activado = true;
+  print(activado);
+  
+//  activado = !activado // si activado es true lo transformara a falso cambio de valor
+    
+  if(activado){
+    print('El motor esta funcionando');
+    
+  } else{
+    print('Desactivado');
+  }
+  
+}
