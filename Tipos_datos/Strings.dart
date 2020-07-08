07e9f2a61a1207e207e8bd7b152d49ffca5877ad
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-08 18:04:32 +0200

    .

---
+++ Tipos_datos/Strings.dart
@@ -0,0 +1,14 @@
+void main(){
+  // Numeros
+  int empleados = 10;
+  double pi = 3.141592;
+  var numero = 1.0; //definicion de presicion
+  
+  print (' $empleados - $pi - $numero');
+  
+  //strings - Cadenas de caracteres
+  String nombre ='Jana';
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length - 1]);
+}
