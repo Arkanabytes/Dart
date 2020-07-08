6b1f63e654ee271ba3e89e9161f4b30b65a60b93
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-08 18:36:19 +0200

    .

---
+++ Tipos_datos/Listas.dart
@@ -0,0 +1,20 @@
+void main(){
+  
+ List<int> numeros = [1,2,3,4,5]; //colecion de informacion que tienen cosas en comun.
+  //<int> es una lista de solo enteros
+ print(numeros);
+  
+  numeros.add(6); // añado un seis
+  print(numeros);
+  
+//numeros.add('Hola mundo'); // añado un seis
+  //print(numeros);
+  
+  //Tamaño fijo
+  List masNumeros = List(10);
+  print( masNumeros );
+ // masNumeros.add(1); //no es correcta esta linea
+  masNumeros[0] = 1;
+  print(masNumeros);
+  
+}
