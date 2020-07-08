af757f0a7665b9dc8539238a0987fd76c6638419
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-08 17:52:19 +0200

    .

---
+++ Tipos_datos/Numero_Strings.dart
@@ -0,0 +1,11 @@
+void main(){
+  
+  /*tres tipos de versiones */
+  
+  // print('Hola mundo');
+  
+  // print ('Hola GLP');
+  
+  var nombre ='Alejandra';
+  print('Hola $nombre'); //mediante injecciones
+}
