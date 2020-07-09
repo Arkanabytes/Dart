263292b84d8b2b2f3ccbbd0afd44e40a7913cb92
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-09 22:54:46 +0200

    .

--- Tipos_datos/Constructores_nombre.dart
+++ Tipos_datos/Constructores_nombre.dart
@@ -7,9 +7,7 @@ void main() {
   Map parsedJson = json.decode( rawJson );
   
 //   print( parsedJson );
-  
   final wolverine = new Heroe.fromJson( parsedJson );
-  
   print(wolverine.nombre);
   print(wolverine.poder);
   
@@ -19,10 +17,8 @@ class Heroe {
   
   String nombre;
   String poder;
-  
   Heroe( this.nombre, this.poder );
-  
-	Heroe.fromJson( Map parsedJson ) {
+  	Heroe.fromJson( Map parsedJson ) {
     nombre = parsedJson['nombre'];
     poder  = parsedJson['poder'];
     
