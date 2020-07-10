96e8461e86b7245f9737c21b5b2b24eda9311543
Alejandra Pinto <cpinto36@alumno.uned.es>
2020-07-10 21:21:54 +0200

    .

--- Tipos_datos/Extends.dart
+++ Tipos_datos/Extends.dart
@@ -3,12 +3,8 @@ void main() {
   final superman = new Heroe();
   superman.nombre = 'Clark Kent';
   final luthor = new Villano();
-  luthor.nombre  = 'Lex Luthor';
-  
-  
+  luthor.nombre  = 'Lex Luthor';  
 }
-
-
 
 abstract class Personaje {
   String poder;
