e287d875085fcfa4358351e51389273e2521bcfa
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 21:51:42 +0200

    .

---
+++ Master_Flutter_Dart/2.-Variables_&_Constants.dart
@@ -0,0 +1,9 @@
+void main(){
+   //var nombre = "Rodrigo";
+  String nombre = "Alejandra";
+  int edad = 30;
+  final double  pi = 3.14; // final por que no cambia con el tiempo
+  print (nombre);
+  print (edad);
+  print (pi);
+}
