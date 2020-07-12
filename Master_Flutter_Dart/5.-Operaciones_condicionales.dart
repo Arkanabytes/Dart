f2e233c6dcd6681a036c02a59ed2850e6c9e0661
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 22:31:29 +0200

    .

---
+++ Master_Flutter_Dart/5.-Operaciones_condicionales.dart
@@ -0,0 +1,32 @@
+//Operaciones condicionales
+ //- == permite condicion o igualdad
+ //- != permite ser diferente igual 
+ //- >  mayor
+ //- <  menor
+ //- >= mayor igual 
+ //- =< menor igual
+
+void main(){
+  
+  
+int numero1 = 20;
+int numero2 = 12;
+  
+print(numero1 == numero2);
+print(numero1 != numero2);
+print(numero1 > numero2);
+print(numero1 < numero2);
+print(numero1 >= numero2);
+print(numero1 <= numero2);
+
+
+  
+  
+//modulos ejemplo 
+  //cuantas veces cae el dos en el 5 una vez el sobrante es restante de una division
+  //se utiliza par o impares en trabajos
+  print(5%2);
+  
+  print (numero1%2 == 0); //respuesta por que no es par
+  
+}
