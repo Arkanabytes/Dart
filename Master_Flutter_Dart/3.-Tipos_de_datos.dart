fabcb6c26b7e51925976589176b15a2ebe82422b
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 22:05:34 +0200

    .

---
+++ Master_Flutter_Dart/3.-Tipos_de_datos.dart
@@ -0,0 +1,22 @@
+//tipos de datos:
+ //-Numbers
+ //-Strings
+ //-Boolean 
+ //-Lists(arrays)
+ //-Maps
+  
+void main(){
+  
+  //Añado valores tipo number
+  var number = 12;
+  var double1 = 15.83;
+  print (number);
+  print (double1);
+  
+  String bienvenido = "Bienvenido a Dart";
+  print(bienvenido.substring(0,11)); //funcion que utilice desde la posicion 0 a 11
+  
+  bool flutterEsGenial = true;
+  print (" Es genial flutter: $flutterEsGenial");
+  
+}
