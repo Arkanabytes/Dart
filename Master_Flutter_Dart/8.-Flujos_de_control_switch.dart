eb6817d99d31c4c2f57ffbbdc1cc9f738c59de02
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 22:57:08 +0200

    .

---
+++ Master_Flutter_Dart/8.-Flujos_de_control_switch.dart
@@ -0,0 +1,36 @@
+//Flujos de control
+ //- If Else
+ //- Switch
+ //- For Loops
+ //- While
+
+void main(){
+  
+String paisHablaHispana = "Chile";
+switch (paisHablaHispana){
+    
+     case "Canada":
+      print ("Incorrecto");
+      break;
+    
+     case "Francia":
+      print ("Incorrecto");
+      break;
+    
+     case "Brazil":
+      print ("Incorrecto");
+      break;
+    
+     case "Eslovenia":
+      print ("Incorrecto");
+      break;
+    
+     case "Chile":
+      print ("Correcto");
+      break;
+     
+  default:
+     throw("Ninguna opcion");
+    break;
+    }
+}
