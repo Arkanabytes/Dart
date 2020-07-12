e9d1c17c680336e1bb31a6159dd80c87c0beb78d
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 22:43:41 +0200

    .

---
+++ Master_Flutter_Dart/6.-Flujos_de_control.dart
@@ -0,0 +1,22 @@
+//Flujos de control
+ //- If Else
+ //- Switch
+ //- For Loops
+ //- While
+
+void main(){
+  
+    bool esFlutterGenial = false;
+  
+  //evaluamos la condicion 
+    if(esFlutterGenial == true){
+      print("Si es genial");
+    }else{
+      print("No es genial");
+    }
+  
+  //otro ejemplo simplificado 
+  String resultado = esFlutterGenial ? "Si es genial" : "No es genial";
+  
+  print(resultado);
+}
