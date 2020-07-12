d76aad568ec743fdf08d3e6c0ea434fcca2a279e
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-12 22:16:17 +0200

    .

---
+++ Master_Flutter_Dart/4.-Operaciones_aritmeticas.dart
@@ -0,0 +1,20 @@
+//Operaciones aritmeticas
+  
+void main(){
+  
+int numero1 = 21;
+int numero2 = 12;
+  
+print(numero1 + numero2);
+print(numero1 - numero2);
+print(numero1 * numero2);
+print(numero1 / numero2);
+  
+//modulos ejemplo 
+  //cuantas veces cae el dos en el 5 una vez el sobrante es restante de una division
+  //se utiliza par o impares en trabajos
+  print(5%2);
+  
+  print (numero1%2 == 0); //respuesta por que no es par
+  
+}
