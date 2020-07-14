5c4238a64c1ba632c1965ef2cf9f5f964d212f6e
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-14 16:59:00 +0200

    .

---
+++ Master_Flutter_Dart/11.-Funciones_definir.dart
@@ -0,0 +1,19 @@
+//Funciones Simples
+void main(){
+  String mensaje = saludos ("Rodrigo");
+  print(mensaje);
+ }
+
+String saludos(String nombre){
+  String mensaje = "Bienvenido $nombre";
+  return mensaje;
+}
+
+
+//ejemlo mas sencillo
+/*void main(){
+  saludos ("Rodrigo");
+ }
+
+void saludos(String nombre)=> print("Bienvenido $nombre");
+}*/
