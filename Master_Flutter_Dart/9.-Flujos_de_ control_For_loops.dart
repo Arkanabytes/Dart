919d47ff2cc90b2705e79a48811947d95d3d6e78
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-14 16:28:27 +0200

    .

---
+++ Master_Flutter_Dart/9.-Flujos_de_ control_For_loops.dart
@@ -0,0 +1,15 @@
+//Flujos de control
+ //- If Else
+ //- Switch
+ //- For Loops
+ //- While
+
+
+//FOR LOOPS//
+void main(){
+  
+  for(var i=0; i<5; i++ ){
+    print("Iteraccion numero $i");
+    } 
+}
+
