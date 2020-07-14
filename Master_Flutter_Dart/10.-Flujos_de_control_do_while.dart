c3a70f495511c461bfb6f1ae52b5c32b989e0741
Alejandra Pinto <copinto-@student.42.us.org>
2020-07-14 16:44:26 +0200

    .

---
+++ Master_Flutter_Dart/10.-Flujos_de_control_do_while.dart
@@ -0,0 +1,18 @@
+//Flujos de control
+ //- If Else
+ //- Switch
+ //- For Loops
+ //- While
+
+
+//WHILES//
+void main(){
+  
+  int iMax = 10;
+  int index = 0;
+  
+  while(index <= iMax){
+    print("Imprimiendo la iteracion $index");
+    index++;
+  }
+}
