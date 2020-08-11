c9b504ff48428ef107ef0c82098d99e65211fd27
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-11 22:33:48 +0200

    .

---
+++ Master_Flutter_Dart/13.-Parámetros_Opcionales_Posicionales.dart
@@ -0,0 +1,14 @@
+//parametros opcionales y posicionales
+
+void main(){
+  saludos("ALEJANDRA", "Pinto", 30);
+}
+
+//siempre entre brakets
+void saludos(String nombre, [String apellido, num edad]){
+   if(apellido !=null && edad != null){
+    print("Bienvenidos $nombre $apellido, edad $edad");
+   }else{
+     print("Bienvenido $nombre");
+   }
+}
