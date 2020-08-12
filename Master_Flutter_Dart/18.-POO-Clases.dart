7a64ecf43f3e640b479d856eec5905011d4c03d7
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-13 01:12:54 +0200

    .

---
+++ Master_Flutter_Dart/18.-POO-Clases.dart
@@ -0,0 +1,28 @@
+/*Arkanabytes*/
+
+class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+  
+  void Arrancar(){
+    print("Hola soy el auto $marca y estoy arrancado");
+  }
+  
+  void CambiarValor(Vehiculo vehiculo){
+    vehiculo.marca = "Mazda";
+  }
+  
+}
+void main(){
+  var auto = new Vehiculo();
+  
+  auto.color = "Rojo";
+  auto.marca = "Ford";
+  auto.Arrancar();
+  
+
+  auto.CambiarValor(auto);
+  auto.Arrancar();
+ 
+}
