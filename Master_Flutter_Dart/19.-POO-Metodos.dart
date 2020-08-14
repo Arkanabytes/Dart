62d8ec41c1fc89913dcdcf618d4250905fc9bac7
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-14 23:42:38 +0200

    .

---
+++ Master_Flutter_Dart/19.-POO-Metodos.dart
@@ -0,0 +1,33 @@
+class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+  
+  void Arrancar(){
+    print("Hola soy el auto $marca y estoy arrancado");
+  }
+  
+  void Acelerar(int velocidad)
+  {
+    print ("Mi velocidad actual es $velocidad km/h");
+  }
+  
+  void CambiarValor(Vehiculo vehiculo){
+    vehiculo.marca ="Mazda";
+  }
+  
+}
+
+void main(){
+  var auto = new Vehiculo();
+  
+  auto.color = "Rojo";
+  auto.marca = "Ford";
+  auto.Arrancar();
+  
+
+  auto.CambiarValor(auto);
+  auto.Arrancar();
+  auto.Acelerar(10);
+ 
+}
