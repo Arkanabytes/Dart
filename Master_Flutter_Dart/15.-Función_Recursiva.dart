e6b44052c8aa521d02bebf38808dabbae0132429
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-15 22:32:10 +0200

    .

--- Master_Flutter_Dart/15.-Función_Recursiva.dart
+++ Master_Flutter_Dart/15.-Función_Recursiva.dart
@@ -1,7 +1,6 @@
 /*Arkanabytes*/
 
-//funcion recursiva 4*3*2*1
-//multiplicamos por el numero anterior
+//funcion recursiva 4*3*2*1 multiplicamos por el numero anterior
 num factorial(num n){
   if(n ==1){
     return 1; //retorna a la funcion
