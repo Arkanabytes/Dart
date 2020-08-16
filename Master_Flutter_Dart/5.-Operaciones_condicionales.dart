662a0d06bbdb7bfeb088deed635118dda85d30b9
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-16 23:28:56 +0200

    .

--- Master_Flutter_Dart/5.-Operaciones_condicionales.dart
+++ Master_Flutter_Dart/5.-Operaciones_condicionales.dart
@@ -20,10 +20,7 @@ print(numero1 >= numero2);
 print(numero1 <= numero2);
 
 //modulos ejemplo 
-  //cuantas veces cae el dos en el 5 una vez el sobrante es restante de una division
-  //se utiliza par o impares en trabajos
+  //cuantas veces cae el dos en el 5 una vez el sobrante es restante de una division se utiliza par o impares en trabajos
   print(5%2);
-  
   print (numero1%2 == 0); //respuesta por que no es par
-  
 }
