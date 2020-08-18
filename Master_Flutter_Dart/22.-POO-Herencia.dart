47b55f6750d1acf53f20188c932078f099a59c6e
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-18 19:57:34 +0200

    .

---
+++ Master_Flutter_Dart/22.-POO-Herencia.dart
@@ -0,0 +1,31 @@
+class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+  
+  void MostrarVehiculo(){
+    print("Marca $marca, Modelo $modelo, color: color");
+    }
+  }
+
+class Camion extends Vehiculo{
+}
+
+class Auto extends Vehiculo{ 
+}
+
+void main(){
+  var camion = new Camion();
+  camion.color = "Blanco";
+  camion.color = "Truck";
+  camion.color = "Volvo";
+  
+  camion.MostrarVehiculo();
+  
+   var auto = new Auto();
+  camion.color = "Rojo";
+  camion.color = "Forcus";
+  camion.color = "Ford";
+  
+  auto.MostrarVehiculo();
+}
