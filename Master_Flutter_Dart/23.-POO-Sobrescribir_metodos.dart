f2d5b6eb81a97fe49d9c116a9d71343049699bb6
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-18 20:57:34 +0200

    .

---
+++ Master_Flutter_Dart/23.-POO-Sobrescribir_metodos.dart
@@ -0,0 +1,38 @@
+class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+  
+  void QueVehiculoSoy(){
+    print("Marca $marca, Modelo $modelo, color: $color");
+    } 
+}
+
+void QueVehiculoSoy(){
+  print("No estoy implementado aun");
+  }
+
+class Camion extends Vehiculo{
+  @override
+  void QueVehiculoSoy(){
+    print("Soy un Camion");
+    
+  }
+}
+
+class Auto extends Vehiculo{ 
+}
+
+void main(){
+  var camion = new Camion();
+  camion.color = "Blanco";
+  camion.color = "Truck";
+  camion.color = "Volvo";
+  camion.QueVehiculoSoy();
+  
+  var auto = new Auto();
+  camion.color = "Rojo";
+  camion.color = "Forcus";
+  camion.color = "Ford";
+  auto.QueVehiculoSoy();
+}
