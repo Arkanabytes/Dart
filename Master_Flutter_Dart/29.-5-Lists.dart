5d71bba30357b3094e364170a5be9eb8d8ec3f62
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-23 22:52:59 +0200

    .

---
+++ Master_Flutter_Dart/29.-5-Lists.dart
@@ -0,0 +1,9 @@
+void main() {
+  var list = [1, 6, 7, 32, 45];
+  
+  print(list.length);
+  
+  for(int i = 0; i < list.length; i++){
+    print("El valor del indice $i es " + list[i].toString());
+  }
+}
