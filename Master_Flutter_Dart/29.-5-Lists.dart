168512a521f3f5f3f7afa32f8b92d3c3b04938cd
Alejandra Pinto <copinto-@student.42.us.org>
2020-08-23 23:17:26 +0200

    .

--- Master_Flutter_Dart/29.-5-Lists.dart
+++ Master_Flutter_Dart/29.-5-Lists.dart
@@ -1,11 +1,37 @@
 /*Arkanabytes*/
 
-void main() {
+class Persona{
+  String nombre;
+}
+
+void main(){
   var list = [1, 6, 7, 32, 45];
-  
   print(list.length);
   
   for(int i = 0; i < list.length; i++){
     print("El valor del indice $i es " + list[i].toString());
   }
+  
+  var onlyString = new List<String>();
+  onlyString.add("Flutter");
+  onlyString.add("Dart");
+  onlyString.add("es genial");
+  
+  for(int i = 0; i < onlyString.length; i++){
+    print(onlyString[i].toString());
+  }
+  
+  var listPersonas = new List<Persona>();
+  var persona1 = new Persona();
+  persona1.nombre = "Alejandra";
+  
+  var persona2 = new Persona();
+  persona2.nombre = "Juan";
+  listPersonas.add(persona1);
+  listPersonas.add(persona2);
+  
+  for(var x in listPersonas){
+    print(x.nombre);
+  }
+  
 }
