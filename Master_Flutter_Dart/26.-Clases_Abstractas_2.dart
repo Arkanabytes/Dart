06e42a249476aa3fb707e370d1bd1476465edd33
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-01 21:23:54 +0200

    .

---
+++ Master_Flutter_Dart/26.-Clases_Abstractas_2.dart
@@ -0,0 +1,13 @@
+abstract class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+}
+
+class Camion extends Vehiculo{
+  
+}
+void main(){
+  var camion = new Camion();
+  camion.color = "Rojo";
+}
