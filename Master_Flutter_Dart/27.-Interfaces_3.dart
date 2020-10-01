518baad3bcaf9ea5ad008050870df5ab45f9de51
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-01 21:43:14 +0200

    .

---
+++ Master_Flutter_Dart/27.-Interfaces_3.dart
@@ -0,0 +1,40 @@
+abstract class Vehiculo{
+  String color;
+  String modelo;
+  String marca;
+  
+  void miCombustible();
+}
+
+abstract class EsTransporte{
+  void QueTransporteSoy();
+}
+class Camion extends Vehiculo implements EsTransporte{
+  void miCombustible(){
+    print("Diesel");
+  }
+  
+}
+
+class Auto extends Vehiculo{
+  void miCombustible(){
+    print("Gasolina");
+  }
+  
+}
+
+class Moto extends Vehiculo{
+  void miCombustible(){
+    print("Petroleo");
+  }
+  
+}
+void main(){
+  var camion = new Camion();
+  var auto = new Auto();
+  var moto = new Moto();
+  camion.miCombustible();
+  auto.miCombustible();
+  moto.miCombustible();
+//  camion.color = "Rojo";
+}
