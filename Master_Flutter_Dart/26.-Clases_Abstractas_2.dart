7a8d5eeee78088fd2b8635867e0117c4ce6c447f
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-01 21:29:28 +0200

    .

--- Master_Flutter_Dart/26.-Clases_Abstractas_2.dart
+++ Master_Flutter_Dart/26.-Clases_Abstractas_2.dart
@@ -2,17 +2,27 @@ abstract class Vehiculo{
   String color;
   String modelo;
   String marca;
+  
+  void miCombustible();
 }
 
 class Camion extends Vehiculo{
-  
+  void miCombustible(){
+    
+  }
 }
 
 class Auto extends Vehiculo{
+  void miCombustible(){
+    
+  }
   
 }
 
 class Moto extends Vehiculo{
+  void miCombustible(){
+    
+  }
   
 }
 void main(){
