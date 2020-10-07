a3dbc3dce07fce433612bce58932b102e226b90f
Alejandra Pinto <copinto-@student.42.us.org>
2020-10-07 22:29:07 +0200

    .

--- Tipos_datos/Funciones.dart
+++ Tipos_datos/Funciones.dart
@@ -3,7 +3,6 @@ void main() {
   String mensaje = saludar2( nombre: 'Fernando', texto: 'Hola,'  );
   print(mensaje);
 }
-
 String saludar({ String texto, String nombre }) {
 //   print('Hola');
   return '$texto $nombre';
